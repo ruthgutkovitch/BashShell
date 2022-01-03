6f941a3f4567be3ae12a6c7d8b6ed8cf30b231d4
Ruth Goutkovitch <75093922+ruthgutkovitch@users.noreply.github.com>
2022-01-03 09:16:36 +0200

    Update smash.cpp

--- smash.cpp
+++ smash.cpp
@@ -12,13 +12,16 @@ int main(int argc, char* argv[]) {
     if(signal(SIGINT , ctrlCHandler)==SIG_ERR) {
         perror("smash error: failed to set ctrl-C handler");
     }
-    //if(signal())
-
-    //TODO: setup sig alarm handler
+    struct sigaction timeOut;
+    timeOut.sa_handler = &alarmHandler;
+    timeOut.sa_flags = SA_RESTART;
+    if(sigaction(SIGALRM,&timeOut,NULL)<0){ //finish
+        perror("smash error: failed to set alarm handler handler");
+    }
 
     SmallShell& smash = SmallShell::getInstance();
     while(true) {
-        std::cout << smash.getName() << " > ";
+        std::cout << smash.getName() << "> ";
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
